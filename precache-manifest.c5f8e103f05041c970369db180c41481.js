84f1cd66edc05ca25d3943e87ba9ce9898928932
alexandrew <andrew.alexandrow97@gmail.com>
2020-04-06 15:36:06 +0300

    Updates

---
+++ precache-manifest.c5f8e103f05041c970369db180c41481.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0921fbf941052223ecc7f4c5e1eebf0b",
+    "url": "/fakebook/index.html"
+  },
+  {
+    "revision": "5741e12aedd2faab3478",
+    "url": "/fakebook/static/css/main.0c2051b4.chunk.css"
+  },
+  {
+    "revision": "e0c70fe02781805ebcbe",
+    "url": "/fakebook/static/js/2.c58d411f.chunk.js"
+  },
+  {
+    "revision": "5741e12aedd2faab3478",
+    "url": "/fakebook/static/js/main.ef061492.chunk.js"
+  },
+  {
+    "revision": "c81aa9d50b9acc405715",
+    "url": "/fakebook/static/js/runtime-main.ee9bd6ca.js"
+  },
+  {
+    "revision": "6d345996df93399ecb443bbbd9808202",
+    "url": "/fakebook/static/media/computer.6d345996.svg"
+  },
+  {
+    "revision": "62f8548abd8d4cb0eaa4765492dc985d",
+    "url": "/fakebook/static/media/cover.62f8548a.jpg"
+  },
+  {
+    "revision": "177e2796b8dafe4054cd51dae27038dc",
+    "url": "/fakebook/static/media/edit.177e2796.svg"
+  },
+  {
+    "revision": "b5afaa0e35d400030fcd4ca4e6364efc",
+    "url": "/fakebook/static/media/error.b5afaa0e.svg"
+  },
+  {
+    "revision": "6f32f036b3299d0123ec9d73bf80b6df",
+    "url": "/fakebook/static/media/facebook.6f32f036.svg"
+  },
+  {
+    "revision": "348c61c826c389892ed2bb12293bf6a1",
+    "url": "/fakebook/static/media/follow.348c61c8.svg"
+  },
+  {
+    "revision": "dfbc3d6e15159cffaff8ef367db93fba",
+    "url": "/fakebook/static/media/github.dfbc3d6e.svg"
+  },
+  {
+    "revision": "f75b91b3d3558f4e4c18f1df4ef3096c",
+    "url": "/fakebook/static/media/globe.f75b91b3.svg"
+  },
+  {
+    "revision": "8fb5a19bf2130e4734c3005302261e12",
+    "url": "/fakebook/static/media/info.8fb5a19b.svg"
+  },
+  {
+    "revision": "a2cb6751dc2982a5a2ba9e31332c78a6",
+    "url": "/fakebook/static/media/instagram.a2cb6751.svg"
+  },
+  {
+    "revision": "abaec5ed4a3cf16c7c3d8080b14b6e39",
+    "url": "/fakebook/static/media/like.abaec5ed.svg"
+  },
+  {
+    "revision": "a9959d080db0ed358be6b0f9c63b8200",
+    "url": "/fakebook/static/media/logo.a9959d08.svg"
+  },
+  {
+    "revision": "7aab935f96e0d1ff54e620f59cda910d",
+    "url": "/fakebook/static/media/magnifier.7aab935f.svg"
+  },
+  {
+    "revision": "d82bd3f64b75f8b50df8a0ef70587561",
+    "url": "/fakebook/static/media/mainlink.d82bd3f6.svg"
+  },
+  {
+    "revision": "acc7eba44ffe09ffb786f6d4a68b46d3",
+    "url": "/fakebook/static/media/onLike.acc7eba4.svg"
+  },
+  {
+    "revision": "087bcc1e260ed3f6df9cd4b0aade460a",
+    "url": "/fakebook/static/media/plus.087bcc1e.svg"
+  },
+  {
+    "revision": "cdda8659adbeb53be484477722d87d50",
+    "url": "/fakebook/static/media/receivedMessage.cdda8659.svg"
+  },
+  {
+    "revision": "61d0e0110977a730eecd75294daca814",
+    "url": "/fakebook/static/media/task.61d0e011.svg"
+  },
+  {
+    "revision": "536ed83104587f420ad5c7aa3c601b7f",
+    "url": "/fakebook/static/media/thumb_broken.536ed831.png"
+  },
+  {
+    "revision": "a9a255ba8705b739e8e0be9a25eb975b",
+    "url": "/fakebook/static/media/twitter.a9a255ba.svg"
+  },
+  {
+    "revision": "d320efd75b27b96a22b3412cd529649c",
+    "url": "/fakebook/static/media/unfollow.d320efd7.svg"
+  },
+  {
+    "revision": "6c534373c7a4dbed322dd0d62bd9cb63",
+    "url": "/fakebook/static/media/unlike.6c534373.svg"
+  },
+  {
+    "revision": "e397e8d249878db9676f8e2179e251a0",
+    "url": "/fakebook/static/media/user.e397e8d2.svg"
+  },
+  {
+    "revision": "f38583d9f91f0a834f1fcbf1ebdb128b",
+    "url": "/fakebook/static/media/vkontakte.f38583d9.svg"
+  },
+  {
+    "revision": "fce087c84f2547603a49490a52b74a86",
+    "url": "/fakebook/static/media/website.fce087c8.svg"
+  },
+  {
+    "revision": "85d35a21079facb93ef85e8ba0f98b9e",
+    "url": "/fakebook/static/media/youtobe.85d35a21.svg"
+  }
+]);
