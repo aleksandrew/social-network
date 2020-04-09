7ac2f1a4b9156f90c6df453af2674f11eda80c5a
Alexandrew <andrew.alexandrow97@gmail.com>
2020-04-09 14:01:08 +0300

    Updates

---
+++ precache-manifest.3edb06e236c0d864aeb8bf5f403cedb4.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b8f92c11ae46f86e2ac812de54af1c",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "551196251ce743b51785",
+    "url": "/social-network/static/css/main.426037be.chunk.css"
+  },
+  {
+    "revision": "7638714002b4b107ca09",
+    "url": "/social-network/static/js/2.f5841ea9.chunk.js"
+  },
+  {
+    "revision": "551196251ce743b51785",
+    "url": "/social-network/static/js/main.e71b211c.chunk.js"
+  },
+  {
+    "revision": "95b54d838c09c180ceb3",
+    "url": "/social-network/static/js/runtime-main.55883228.js"
+  },
+  {
+    "revision": "6d345996df93399ecb443bbbd9808202",
+    "url": "/social-network/static/media/computer.6d345996.svg"
+  },
+  {
+    "revision": "62f8548abd8d4cb0eaa4765492dc985d",
+    "url": "/social-network/static/media/cover.62f8548a.jpg"
+  },
+  {
+    "revision": "177e2796b8dafe4054cd51dae27038dc",
+    "url": "/social-network/static/media/edit.177e2796.svg"
+  },
+  {
+    "revision": "b5afaa0e35d400030fcd4ca4e6364efc",
+    "url": "/social-network/static/media/error.b5afaa0e.svg"
+  },
+  {
+    "revision": "6f32f036b3299d0123ec9d73bf80b6df",
+    "url": "/social-network/static/media/facebook.6f32f036.svg"
+  },
+  {
+    "revision": "348c61c826c389892ed2bb12293bf6a1",
+    "url": "/social-network/static/media/follow.348c61c8.svg"
+  },
+  {
+    "revision": "dfbc3d6e15159cffaff8ef367db93fba",
+    "url": "/social-network/static/media/github.dfbc3d6e.svg"
+  },
+  {
+    "revision": "f75b91b3d3558f4e4c18f1df4ef3096c",
+    "url": "/social-network/static/media/globe.f75b91b3.svg"
+  },
+  {
+    "revision": "8fb5a19bf2130e4734c3005302261e12",
+    "url": "/social-network/static/media/info.8fb5a19b.svg"
+  },
+  {
+    "revision": "a2cb6751dc2982a5a2ba9e31332c78a6",
+    "url": "/social-network/static/media/instagram.a2cb6751.svg"
+  },
+  {
+    "revision": "abaec5ed4a3cf16c7c3d8080b14b6e39",
+    "url": "/social-network/static/media/like.abaec5ed.svg"
+  },
+  {
+    "revision": "a9959d080db0ed358be6b0f9c63b8200",
+    "url": "/social-network/static/media/logo.a9959d08.svg"
+  },
+  {
+    "revision": "7aab935f96e0d1ff54e620f59cda910d",
+    "url": "/social-network/static/media/magnifier.7aab935f.svg"
+  },
+  {
+    "revision": "d82bd3f64b75f8b50df8a0ef70587561",
+    "url": "/social-network/static/media/mainlink.d82bd3f6.svg"
+  },
+  {
+    "revision": "acc7eba44ffe09ffb786f6d4a68b46d3",
+    "url": "/social-network/static/media/onLike.acc7eba4.svg"
+  },
+  {
+    "revision": "087bcc1e260ed3f6df9cd4b0aade460a",
+    "url": "/social-network/static/media/plus.087bcc1e.svg"
+  },
+  {
+    "revision": "cdda8659adbeb53be484477722d87d50",
+    "url": "/social-network/static/media/receivedMessage.cdda8659.svg"
+  },
+  {
+    "revision": "61d0e0110977a730eecd75294daca814",
+    "url": "/social-network/static/media/task.61d0e011.svg"
+  },
+  {
+    "revision": "536ed83104587f420ad5c7aa3c601b7f",
+    "url": "/social-network/static/media/thumb_broken.536ed831.png"
+  },
+  {
+    "revision": "a9a255ba8705b739e8e0be9a25eb975b",
+    "url": "/social-network/static/media/twitter.a9a255ba.svg"
+  },
+  {
+    "revision": "d320efd75b27b96a22b3412cd529649c",
+    "url": "/social-network/static/media/unfollow.d320efd7.svg"
+  },
+  {
+    "revision": "6c534373c7a4dbed322dd0d62bd9cb63",
+    "url": "/social-network/static/media/unlike.6c534373.svg"
+  },
+  {
+    "revision": "e397e8d249878db9676f8e2179e251a0",
+    "url": "/social-network/static/media/user.e397e8d2.svg"
+  },
+  {
+    "revision": "f38583d9f91f0a834f1fcbf1ebdb128b",
+    "url": "/social-network/static/media/vkontakte.f38583d9.svg"
+  },
+  {
+    "revision": "fce087c84f2547603a49490a52b74a86",
+    "url": "/social-network/static/media/website.fce087c8.svg"
+  },
+  {
+    "revision": "85d35a21079facb93ef85e8ba0f98b9e",
+    "url": "/social-network/static/media/youtobe.85d35a21.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.48d86f86ce9ac1fefa7797f970f7a43b.js"
+  "/social-network/precache-manifest.3edb06e236c0d864aeb8bf5f403cedb4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-network/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
