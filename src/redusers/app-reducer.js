8f5b3ace290b33a53a1dad5ecb1e42f5c240548d
Alexandrew <andrew.alexandrow97@gmail.com>
2020-04-21 15:08:57 +0300

    Add to project typescript

--- src/redusers/app-reducer.js
+++ src/redusers/app-reducer.js
@@ -1,36 +1,35 @@
+"use strict";
 // outsource dependencies
-
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // local dependencies
-import { APP } from '../constans/types';
-
-const initialState = {
+var types_1 = require("../constans/types");
+var initialState = {
     initialized: false,
     errorMessages: null,
 };
-
-export const selector = (state) => state.app;
-
-const appReducer = (state = initialState, action) => {
+exports.selector = function (state) { return state.app; };
+var appReducer = function (state, action) {
+    if (state === void 0) { state = initialState; }
     switch (action.type) {
-        case APP.INITIALIZED_SUCCESS:
-
-            return {
-                ...state,
-                initialized: true,
-            };
-
-        case APP.SET_ERROR_MESSAGES: {
-            const { messages } = action;
-
-            return {
-                ...state,
-                errorMessages: messages,
-            };
+        case types_1.APP.INITIALIZED_SUCCESS:
+            return __assign(__assign({}, state), { initialized: true });
+        case types_1.APP.SET_ERROR_MESSAGES: {
+            var messages = action.messages;
+            return __assign(__assign({}, state), { errorMessages: messages });
         }
-
         default:
             return state;
     }
 };
-
-export default appReducer;
+exports.default = appReducer;
